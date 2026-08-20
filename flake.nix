73e0c2dcc095b5480513607d85f967c20c7f1008
Neovimproved <neovimproved@yahoo.com.(none)>
2026-08-20 20:47:37 +0100

    chore: make flake cross-platform

--- flake.nix
+++ flake.nix
@@ -7,33 +7,45 @@
 
   outputs =
     { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
     {
-      packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
-        pname = "mcserver";
-        version = "0.2.3";
-        cargoLock.lockFile = ./Cargo.lock;
-        src = pkgs.lib.cleanSource ./.;
+      packages = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" "hello-linux" ] (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.rustPlatform.buildRustPackage {
+            pname = "mcserver";
+            version = "0.2.3";
+            cargoLock.lockFile = ./Cargo.lock;
+            src = pkgs.lib.cleanSource ./.;
 
-        nativeBuildInputs = with pkgs; [
-          pkg-config
-        ];
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+            ];
 
-        buildInputs = with pkgs; [
-          openssl
-        ];
-      };
+            buildInputs = with pkgs; [
+              openssl
+            ];
+          };
+        }
+      );
 
-      devShells.${system}.default = pkgs.mkShell {
-        # A recent stable rust toolchain will be added
-        inputsFrom = [ self.packages.${system}.default ];
+      devShells = nixpkgs.lib.genAttrs [ "x86-64_linux" "aarch64-linux" "hello-linux" ] (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = {
+            # A recent stable rust toolchain will be added
+            inputsFrom = [ self.packages.${system}.default ];
 
-        nativeBuildInputs = with pkgs; [
-          rust-analyzer
-        ];
-      };
+            nativeBuildInputs = with pkgs; [
+              rust-analyzer
+            ];
+          };
+        }
+      );
     };
 }
